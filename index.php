ea9b60732db53a84eb903d48f699e9d3add5572e
Shashi Bhushan <shashibhushan86@yahoo.com>
2025-08-10 17:42:07 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,226 @@
-<html>
-    <h1>Hello world</h1>
-<html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">MapleNyx</a>
+    </div>
+    <ul class="nav navbar-nav navbar-right">
+      <li class="active"><a href="#">Home</a></li>
+      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1 <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Page 1-1</a></li>
+          <li><a href="#">Page 1-2</a></li>
+          <li><a href="#">Page 1-3</a></li>
+        </ul>
+      </li>
+      <li><a href="#">Page 2</a></li>
+      <li><a href="#">Page 3</a></li>
+    </ul>
+  </div>
+</nav>
+  
+<div class="container">
+  <h3>Navbar With Dropdown</h3>
+  <p>This example adds a dropdown menu for the "Page 1" button in the navigation bar.</p>
+</div>
+
+<!-- wp:template-part {"slug":"header-hero","theme":"pub/assembler"} /-->
+
+<!-- wp:group {"tagName":"main","metadata":{"name":"Main"},"style":{"spacing":{"blockGap":"0","margin":{"top":"0"}}},"layout":{"type":"default"}} -->
+<main class="wp-block-group" style="margin-top:0">
+    <!-- wp:post-content {"layout":{"type":"constrained"}} /-->
+</main>
+<!-- /wp:group -->
+ 
+ <!-- wp:template-part {"slug":"footer","theme":"pub/assembler"} /-->
+
+
+<!-- wp:group {"metadata":{"name":"General content","patternCategory":"general","remotePatternId":"17736-general"},"align":"full","className":"alignfull is-style-default","style":{"spacing":{"margin":{"top":"0","bottom":"0"},"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}}},"layout":{"type":"constrained","justifyContent":"center"}} -->
+<div class="wp-block-group alignfull is-style-default" id="general-section" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40)"><!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|50","left":"var:preset|spacing|40"}}}} -->
+<div class="wp-block-columns alignwide"><!-- wp:column {"verticalAlignment":"center"} -->
+<div class="wp-block-column is-vertically-aligned-center"><!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"},"blockGap":"16px"},"dimensions":{"minHeight":"100%"}},"layout":{"type":"flex","orientation":"vertical","verticalAlignment":"center","justifyContent":"center","flexWrap":"wrap"}} -->
+<div class="wp-block-group alignfull" style="min-height:100%;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:paragraph -->
+<p>MapleNyx specializes in delivering tailored IT consulting services to meet your unique business needs. Our expertise covers data solutions, web development, and Murex consulting, ensuring your technology infrastructure supports your growth and innovation goals.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"textAlign":"center"} -->
+<h2 class="wp-block-heading has-text-align-center">Expertise in IT Consulting</h2>
+<!-- /wp:heading -->
+
+<!-- wp:spacer {"height":"0px","style":{"layout":{"flexSize":"4px","selfStretch":"fixed"}}} -->
+<div style="height:0px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"className":"is-style-fill"} -->
+<div class="wp-block-button is-style-fill"><a class="wp-block-button__link wp-element-button">Contact Us</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"aspectRatio":"3/4","scale":"cover","sizeSlug":"full","linkDestination":"none","style":{"color":[]}} -->
+<figure class="wp-block-image size-full"><img src="https://sbvgooogle-ykdna.wordpress.com/wp-content/uploads/2025/07/it-consulting-8.jpeg" alt="The image depicts a row of tall server cabinets in a data center or server room, with white cables neatly organized and se..." style="aspect-ratio:3/4;object-fit:cover"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"metadata":{"categories":["services"],"name":"Services Offered","patternName":"assembler/services-2","patternCategory":"services","remotePatternId":13602},"align":"full","className":"alignfull is-style-default","style":{"spacing":{"margin":{"top":"0","bottom":"0"},"blockGap":"var:preset|spacing|30","padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"layout":{"type":"constrained","justifyContent":"center"}} -->
+<div class="wp-block-group alignfull is-style-default" id="services-section" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50)"><!-- wp:heading {"align":"wide","style":{"layout":{"selfStretch":"fixed","flexSize":"100%"}}} -->
+<h2 class="wp-block-heading alignwide">Services</h2>
+<!-- /wp:heading -->
+
+<!-- wp:group {"metadata":{"name":"Sections"},"align":"wide","style":{"spacing":{"margin":{"top":"0","bottom":"0"}}}} -->
+<div class="wp-block-group alignwide" style="margin-top:0;margin-bottom:0"><!-- wp:group {"metadata":{"name":"Section"},"align":"wide"} -->
+<div class="wp-block-group alignwide"><!-- wp:spacer {"height":"20px"} -->
+<div style="height:20px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:columns {"metadata":{"name":"Items"},"align":"wide","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|60","left":"var:preset|spacing|30"}}}} -->
+<div class="wp-block-columns alignwide"><!-- wp:column {"metadata":{"name":"Item"},"style":{"spacing":{"blockGap":"0"}}} -->
+<div class="wp-block-column"><!-- wp:group {"style":{"spacing":{"blockGap":"24px"}},"layout":{"type":"default"}} -->
+<div class="wp-block-group"><!-- wp:heading {"level":3,"className":"is-service-name"} -->
+<h3 class="wp-block-heading is-service-name">Data Solutions</h3>
+<!-- /wp:heading -->
+
+<!-- wp:image {"aspectRatio":"4/3","scale":"cover","sizeSlug":"large","linkDestination":"none","style":{"color":{"duotone":"unset"}}} -->
+<figure class="wp-block-image size-large"><img src="https://sbvgooogle-ykdna.wordpress.com/wp-content/uploads/2025/07/it-consulting-9.jpeg" alt="The image depicts a computer screen displaying a stock market or financial data visualization, likely in an office setting..." style="aspect-ratio:4/3;object-fit:cover"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph {"className":"is-service-description"} -->
+<p class="is-service-description">Providing comprehensive data analysis, management, and consultancy services.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"className":"is-service-link"} -->
+<div class="wp-block-button is-service-link"><a class="wp-block-button__link wp-element-button">Learn More</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"metadata":{"name":"Item"},"style":{"spacing":{"blockGap":"0"}}} -->
+<div class="wp-block-column"><!-- wp:group {"style":{"spacing":{"blockGap":"24px"}},"layout":{"type":"default"}} -->
+<div class="wp-block-group"><!-- wp:heading {"level":3,"className":"is-service-name"} -->
+<h3 class="wp-block-heading is-service-name">Web Dev Services</h3>
+<!-- /wp:heading -->
+
+<!-- wp:image {"aspectRatio":"4/3","scale":"cover","sizeSlug":"large","linkDestination":"none","style":{"color":{"duotone":"unset"}}} -->
+<figure class="wp-block-image size-large"><img src="https://sbvgooogle-ykdna.wordpress.com/wp-content/uploads/2025/07/it-consulting-11.jpeg" alt="A monitor displaying lines of code and a stack of folders, with the words 'Deployment' written in white and centered." style="aspect-ratio:4/3;object-fit:cover"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph {"className":"is-service-description"} -->
+<p class="is-service-description">Creating and maintaining responsive, user-friendly websites.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"className":"is-service-link"} -->
+<div class="wp-block-button is-service-link"><a class="wp-block-button__link wp-element-button">Explore Now</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"metadata":{"name":"Item"},"style":{"spacing":{"blockGap":"0"}}} -->
+<div class="wp-block-column"><!-- wp:group {"style":{"spacing":{"blockGap":"24px"}},"layout":{"type":"default"}} -->
+<div class="wp-block-group"><!-- wp:heading {"level":3,"className":"is-service-name"} -->
+<h3 class="wp-block-heading is-service-name">Murex Consulting</h3>
+<!-- /wp:heading -->
+
+<!-- wp:image {"aspectRatio":"4/3","scale":"cover","sizeSlug":"large","linkDestination":"none","style":{"color":{"duotone":"unset"}}} -->
+<figure class="wp-block-image size-large"><img src="https://sbvgooogle-ykdna.wordpress.com/wp-content/uploads/2025/07/it-consulting-1.jpeg" alt="The file has reached the synchronization point and is currently undergoing the synchronization process." style="aspect-ratio:4/3;object-fit:cover"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph {"className":"is-service-description"} -->
+<p class="is-service-description">Specialized consulting services for Murex trading and risk management systems.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"className":"is-service-link"} -->
+<div class="wp-block-button is-service-link"><a class="wp-block-button__link wp-element-button">Explore Services</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"metadata":{"categories":["services"],"name":"Testimonials","patternName":"assembler/services-5","patternCategory":"testimonials","remotePatternId":15246},"align":"full","className":"alignfull is-style-default","style":{"spacing":{"margin":{"top":"0","bottom":"0"},"padding":{"top":"var:preset|spacing|80","bottom":"var:preset|spacing|80"},"blockGap":"var:preset|spacing|30"}},"layout":{"type":"constrained","justifyContent":"center","wideSize":"1100px"}} -->
+<div class="wp-block-group alignfull is-style-default" id="testimonials-section" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--80);padding-bottom:var(--wp--preset--spacing--80)"><!-- wp:image {"width":"72px","height":"72px","scale":"cover","sizeSlug":"full","linkDestination":"none","align":"center","className":"is-style-rounded"} -->
+<figure class="wp-block-image aligncenter size-full is-resized is-style-rounded"><img src="https://dotcompatterns.wordpress.com/wp-content/uploads/2024/09/2-1.jpg" alt="" style="object-fit:cover;width:72px;height:72px"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"textAlign":"center","align":"wide","className":"is-testimonial-review"} -->
+<h2 class="wp-block-heading alignwide has-text-align-center is-testimonial-review">Their data solutions transformed our business processes, making us more efficient and competitive.</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center","metadata":{"name":"Name"},"className":"is-testimonial-name"} -->
+<p class="has-text-align-center is-testimonial-name">Testimonial generated by AI</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"metadata":{"name":"Contact","patternCategory":"contact","remotePatternId":13498},"align":"full","className":"alignfull is-style-section-1","style":{"spacing":{"margin":{"top":"0","bottom":"0"},"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"},"blockGap":"var:preset|spacing|30"}},"layout":{"type":"constrained","justifyContent":"center"}} -->
+<div class="wp-block-group alignfull is-style-section-1" id="contact-section" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50)"><!-- wp:columns {"align":"wide"} -->
+<div class="wp-block-columns alignwide"><!-- wp:column {"width":"50%"} -->
+<div class="wp-block-column" style="flex-basis:50%"><!-- wp:heading -->
+<h2 class="wp-block-heading">Visit us</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"className":"is-contact-description"} -->
+<p class="is-contact-description">Contact us to learn more about our Data Solutions, Web Development, and Murex Consulting services. We're here to assist with your IT needs.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"40%"} -->
+<div class="wp-block-column" style="flex-basis:40%"></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:columns {"align":"wide"} -->
+<div class="wp-block-columns alignwide"><!-- wp:column {"verticalAlignment":"top","width":""} -->
+<div class="wp-block-column is-vertically-aligned-top"><!-- wp:paragraph {"align":"left","className":"is-contact-address-phone"} -->
+<p class="has-text-align-left is-contact-address-phone">Toronto, Canada
+(416) 555-1234</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:social-links {"size":"has-normal-icon-size","className":"is-style-logos-only","style":{"spacing":{"blockGap":{"top":"16px","left":"16px"}}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<ul class="wp-block-social-links has-normal-icon-size is-style-logos-only"><!-- wp:social-link {"url":"https://facebook.com/","service":"facebook"} /-->
+
+<!-- wp:social-link {"url":"https://twitter.com","service":"twitter"} /-->
+
+<!-- wp:social-link {"url":"https://instagram.com","service":"instagram"} /--></ul>
+<!-- /wp:social-links -->
+
+<!-- wp:spacer {"height":"var:preset|spacing|10"} -->
+<div style="height:var(--wp--preset--spacing--10)" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":""} -->
+<div class="wp-block-column"><!-- wp:group {"style":{"dimensions":{"minHeight":600}}} -->
+<div class="wp-block-group" style="min-height:600px"><!-- wp:jetpack/map {"align":"full","points":[{"placeTitle":"Toronto, Canada","title":"MapleNyx Consulting Partners Ltd","caption":"","id":"place.0.2639881245073994","coordinates":{"latitude":43.647938,"longitude":-79.38355}}],"zoom":12,"mapCenter":{"lat":43.647923945854515,"lng":-79.38355000000001},"markerColor":"#151515","mapHeight":600,"className":"is-style-satellite"} -->
+<div class="wp-block-jetpack-map is-style-satellite alignfull" data-map-style="satellite" data-map-details="true" data-points="[{&quot;placeTitle&quot;:&quot;Toronto, Canada&quot;,&quot;title&quot;:&quot;MapleNyx Consulting Partners Ltd&quot;,&quot;caption&quot;:&quot;&quot;,&quot;id&quot;:&quot;place.0.2639881245073994&quot;,&quot;coordinates&quot;:{&quot;latitude&quot;:43.647938,&quot;longitude&quot;:-79.38355}}]" data-zoom="12" data-map-center="{&quot;lat&quot;:43.647923945854515,&quot;lng&quot;:-79.38355000000001}" data-marker-color="#151515" data-map-height="600" data-show-fullscreen-button="true"><ul><li><a href="https://www.google.com/maps/search/?api=1&amp;query=43.647938,-79.38355">MapleNyx Consulting Partners Ltd</a></li></ul></div>
+<!-- /wp:jetpack/map --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
+
+
+</body>
+</html>
